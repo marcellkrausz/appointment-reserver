1ed74a589f0ee179cc7edbb6d5ca416a25d179a4
marcellkrausz <krauszmaarcell@gmail.com>
2021-07-13 10:11:12 +0200

    Initial commit

---
+++ src/test/java/com/marcellkrausz/appointmentreserver/AppointmentReserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcellkrausz.appointmentreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentReserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

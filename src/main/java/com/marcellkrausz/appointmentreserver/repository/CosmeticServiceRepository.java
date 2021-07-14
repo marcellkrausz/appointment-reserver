4c60c6b9b1679247213feaed2334bd8735212b6a
marcellkrausz <krauszmaarcell@gmail.com>
2021-07-14 18:52:41 +0200

    add repositories

---
+++ src/main/java/com/marcellkrausz/appointmentreserver/repository/CosmeticServiceRepository.java
@@ -0,0 +1,7 @@
+package com.marcellkrausz.appointmentreserver.repository;
+
+import com.marcellkrausz.appointmentreserver.model.CosmeticService;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CosmeticServiceRepository extends CrudRepository<CosmeticService, Long> {
+}
---
+++ src/main/java/com/marcellkrausz/appointmentreserver/repository/Customer.java
@@ -0,0 +1,6 @@
+package com.marcellkrausz.appointmentreserver.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Customer extends CrudRepository<Customer, Long> {
+}
